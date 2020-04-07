4c3c4ce9eedbf7d8a98e4854e3c7d61008b76603
yyywang <ywang_perfect@163.com>
2020-04-07 15:23:45 +0800

    config domain

---
+++ app/config/secure.py
@@ -0,0 +1,42 @@
+"""
+    :copyright: © 2019 by the Lin team.
+    :license: MIT, see LICENSE for more details.
+"""
+
+# 安全性配置
+from app.config.setting import BaseConfig
+
+
+class DevelopmentSecure(BaseConfig):
+    """
+    开发环境安全性配置
+    """
+    SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:python0096@localhost:3306/lin-cms'
+
+    SQLALCHEMY_ECHO = False
+
+    SECRET_KEY = '\x88W\xf09\x91\x07\x98\x89\x87\x96\xa0A\xc68\xf9\xecJJU\x17\xc5V\xbe\x8b\xef\xd7\xd8\xd3\xe6\x95*4'
+
+    WX_APP_ID = 'wx77e07d7773c8baba'
+
+    WX_APP_SECRET = 'b4486877733d3bb9a648c1c320a96051'
+
+    SITE_DOMAIN = "https://www.znldj.com"
+
+
+
+class ProductionSecure(BaseConfig):
+    """
+    生产环境安全性配置
+    """
+    SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:MyNewPassword@localhost:3306/lincms'
+
+    SQLALCHEMY_ECHO = False
+
+    SECRET_KEY = '\x88W\xf09\x91\x07\x98\x89\x87\x96\xa0A\xc68\xf9\xecJJU\x17\xc5V\xbe\x8b\xef\xd7\xd8\xd3\xe6\x95*4'
+
+    WX_APP_ID = 'wx77e07d7773c8baba'
+
+    WX_APP_SECRET = 'b4486877733d3bb9a648c1c320a96051'
+
+    SITE_DOMAIN = "https://www.znldj.com"
